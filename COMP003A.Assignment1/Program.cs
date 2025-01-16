542ffcaaba1ee2f1ab11a2ccc506fbab95a361d0
SalinasJason <jasonsalinas770@gmail.com>
2025-01-15 23:30:56 -0800

    Added 10 facts about me

--- COMP003A.Assignment1/Program.cs
+++ COMP003A.Assignment1/Program.cs
@@ -1,12 +1,25 @@
-﻿namespace COMP003A.Assignment1
+﻿
+namespace COMP003A.Assignment1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your name:");
+            Console.WriteLine("Enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello, " + name +"! My name is Jason Salinas.");
+
+            Console.WriteLine("Here are 10 things about me:");
+            Console.WriteLine("1. I'm 20 years old");
+            Console.WriteLine("2. I'm Mexican");
+            Console.WriteLine("3. I enjoy watching football and basketball");
+            Console.WriteLine("4. My favorite football team is the San Francisco 49ers");
+            Console.WriteLine("5. My favorite basketball team is the Philadelphia 76ers");
+            Console.WriteLine("6. I like to workout");
+            Console.WriteLine("7. I like cars");
+            Console.WriteLine("8. I like playing video games");
+            Console.WriteLine("9. I used to be a welder");
+            Console.WriteLine("10. My favorite color is grey");
         }
     }
 }
